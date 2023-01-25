facfb2e5a93a726629c30420f814288b139c8416
George S <george.soulv@gmail.com>
2023-01-25 21:34:06 +0300

    2 модуль

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,20 @@
 let calculator = {
-  // ваш код
+  first: 0,
+  second: 0,
+  read(a, b) {
+    first = a;
+    second = b;
+  },
+  sum() {
+    return first + second;
+  },
+  mul() {
+    return first * second;
+  }  
 };
 
+calculator.read(3, 5);
+console.log(calculator.sum());
+
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
-window.calculator = calculator; // делает ваш калькулятор доступным глобально
+//window.calculator = calculator; // делает ваш калькулятор доступным глобально
